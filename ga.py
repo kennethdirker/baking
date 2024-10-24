1697e353614a25555e1e4ef2bea0124f9d0d5d00
Kenneth Dirker <kennethdirker@hotmail.nl>
2024-10-24 15:05:50 +0200

    .

--- ga.py
+++ ga.py
@@ -29,6 +29,10 @@ INGREDIENT_FUNCTION: list[str] = [
 
 
 class Ingredient:
+    """
+    
+    """
+
     def __init__(
             self,
             name:       str,
@@ -48,13 +52,21 @@ class Ingredient:
         self.function_: str = function_     # Function of ingredient
         self.power: float = power           # Weak taste - [0:100] - Strong taste
 
-    def mutate(self):
+
+    def mutate(self, delta: float = 1):
+        """
+        
+        """
         random_factor = random.gauss(1, 0.05)    # TODO experiment with standard deviation
         self.quantity = self.quantity ** random_factor
         
 
 
 class Recipe:
+    """
+    
+    """
+
     def __init__(
             self,
             target_taste: str = None,
@@ -72,39 +84,91 @@ class Recipe:
 
         self.moistness: float = -1   # Desert - [0:100] - Ocean
 
+
     def add_ingredient(self, ingredient: Ingredient):
+        """
+        
+        """
         self.ingredients.append(ingredient)
 
+
     def remove_ingredient(self):
+        """
+        
+        """
+        size = len(self.ingredients)
+        if size > 0:
+            self.ingredients.pop(random.randint(0, size - 1))
 
 
     def length(self) -> int:
+        """
+        
+        """
         return len(self.ingredients)
 
-    def evaluate
+
+    def evaluate(self) -> float:
+        """
+        
+        """
+        ret = -1
+        return ret
 
 
+class GA:
+    def __init__(self):
+        """
+        
+        """
+        ...
 
-def init():
-    ...
+    def _selection(self):
+        """
+        
+        """
 
-def selection():
-    ...
 
-def crossover():
-    ...
+    def _crossover(self):
+        """
+        
+        """
 
-def mutation():
-    ...
 
-def normalize():
-    ...
+    def _mutation(self):
+        """
+        
+        """
 
+
+    def _normalize(self):
+        """
+        
+        """
+
+        
+    def run(self):
+        """
+        
+        """
+        # TODO Select
+        # TODO Crossover
+        # TODO Mutation
+        # TODO Normalization
 
 
 
 def main():
-    dataset = init()
+    # TODO Initialize dataset of ingredients
+    dataset = ...
+
+    # Parameters that tune the genetic algorithm
+    population_size = 20    # Amount of created children each epoch
+    selection_size  = 10    # Amount of selected children to advance
+    mutation_delta  = 1     
+
+    ga = GA()
+
 
 
 if __name__ == "__main__":
