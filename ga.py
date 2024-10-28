f3c66d325a1922d66e6abfc4bfa0c4d7c06b7144
Kenneth Dirker <kennethdirker@hotmail.nl>
2024-10-28 21:14:10 +0100

    evaluation function

--- ga.py
+++ ga.py
@@ -1,28 +1,39 @@
-import typing, random, time, math
+import typing, random, time, math, json
 from typing import List
 
-# TODO Augment
 TASTES: List[str] = [
-    "sour",
-    "sweet", 
-    "salty", 
-    "bitter", 
-    "herby",  
+    "bitter",
+    "chocolate", 
+    "coffee", 
+    "creamy", 
+    "floral",
+    "fresh",
+    "fruity",
+    "mealy",
+    "neutral",
+    "nutty",
+    "salty",
+    "spicy",
+    "sweet",
+    "tangy",
+    "tropical"  
 ] 
 
-# TODO Augment
-INGREDIENT_FUNCTION: List[str] = [
-    "base",
-    "binder",
-    "topping",
-    "enhancer",
-    "texture",
-]
+INGREDIENT_FUNCTIONS: dict[str, int] = {
+    "fat": 3,
+    "sweetener": 3,
+    "binding agent": 4,
+    "flavoring": 5,
+    "dry ingredient": 1,
+    "mix-in": 3,
+    "raising agent": 5,
+    "moisture agent": 2
+}
 
 
 class Ingredient:
     """
-    
+    Class that represents an ingredient in a recipe.
     """
     def __str__(self):
         return f"{self.name}, {float(self.quantity):.4} {self.unit}, {self.moistness}, {self.taste}, {self.function_}, {self.intensity}"
@@ -30,35 +41,39 @@ class Ingredient:
     def __init__(
             self,
             name:       str,
-            quantity:   float,
-            unit:       str,
+            quantity:   float = -1,
+            unit:       str = "g",
             moistness:  float = -1,
-            taste:      str = "",
+            taste:      List[str] = "",
             function_:  str = "",
             intensity:  float = -1,
         ):
         self.name: str = name
-        self.quantity: float = quantity     # Numerical amount to add
+        if quantity == -1:
+            self.quantity: float = random.randint(1, 1000)
+        else:
+            self.quantity: float = quantity # Numerical amount to add
         self.unit: str = unit               # Unit of measurement (g, l, kg, etc.)
         self.moistness: float = moistness   # Desert - [0:10] - Ocean
-        self.taste: str = taste   # Salty, bitter, etc...
+        self.taste: str = taste             # Salty, bitter, etc...
         self.function_: str = function_     # Function of ingredient
-        self.intensity: float = intensity           # Weak taste - [0:10] - Strong taste
+        self.intensity: float = intensity   # Weak taste - [0:10] - Strong taste
 
 
     def mutate(self, delta: float = 1):
         """
-        
+        Mutate the quantity of the ingredient.
         """
-        # TODO test
-        random_factor = random.gauss(1, 0.05)    # TODO experiment with standard deviation
-        self.quantity = self.quantity ** random_factor        
+        if self.quantity > 0:
+            random_factor = random.gauss(1, 0.05)    # TODO experiment with standard deviation
+            self.quantity = self.quantity ** random_factor        
 
 
 class Recipe:
     """
     Class that represents a cookie recipe.
     """
+
     # Ingredient mutation types
     mut_type = ["add", "del", "mut"]
 
@@ -85,7 +100,8 @@ class Recipe:
 
     def merge_ingredients(self):
         """
-        
+        Merge doubly listed ingredients into one ingredient by
+        aggregating the ingredient quantities.
         """
         d = {}
         merged_ingredients = []
@@ -114,9 +130,9 @@ class Recipe:
 
     def mutate(self, db: List[Ingredient], mutation_rate: float = 0.8):
         """
-        
+        Potentially mutate the recipe by adding/removing an ingredient or
+        changing the quantity of ingredients.
         """
-        # TODO test
         # Are we going to mutate?
         r: float = random.random()
         if r < mutation_rate:
@@ -136,7 +152,6 @@ class Recipe:
                 for i in self.ingredients:
                     if to_add.name == i.name:
                         done = False
-                    
                 
             self.ingredients.append(random.choice(db))
             
@@ -151,9 +166,6 @@ class Recipe:
             # Decide amount of mutations to do
             mutations = random.gauss(0, 3)      # TODO tweak OR why not just do 1!
             mutations: int = abs(math.ceil(r))
-            # size = len(self.ingredients)
-            # if mutations > size:
-                # mutations = size
 
             # Decide which ingredients to mutate and execute
             mutants = random.sample(self.ingredients, min(mutations, len(self.ingredients)))
@@ -164,29 +176,41 @@ class Recipe:
 
     def _moistness(self) -> float:
         """
-        
+        Calculate how moist the resulting mesh of ingredients might be.
+
+        Return a moistness score in the range: Desert - [0:10] - Ocean
         """
-        # TODO Test
-        moistness = 0      # Desert - [0:10] - Ocean
-        # [print(type(i), i) for i in self.ingredients]
-        parts = [(i.quantity / 1000, i.moistness) for i in self.ingredients]
-        for p in parts:
-            moistness += p[0] * p[1]
-        return moistness
+        # moistness = 0      # Desert - [0:10] - Ocean
+        # parts = [(i.quantity / 1000, i.moistness) for i in self.ingredients]
+        # for p in parts:
+        #     moistness += p[0] * p[1]
+        # print(f"moistness: {moistness}")
+        # return moistness
+        return sum([i.quantity * i.moistness / 1000 for i in self.ingredients])
+
+    def _intensity(self) -> float:
+        """
+        Calculate how intense the taste of the resulting mesh of ingredients 
+        might be.
+
+        Return a intensity score in the range: Water - [0:10] - Pure salt
+        """
+        # intensity = 0      # Water - [0:10] - Pure salt
+        # parts = [(i.quantity / 1000, i.intensity) for i in self.ingredients]
+        # for p in parts:
+            # intensity += p[0] * p[1]
+        # return intensity
+        return sum([i.quantity * i.intensity / 1000 for i in self.ingredients])
 
 
     def length(self) -> int:
-        """
-        
-        """
         return len(self.ingredients)
     
 
     def normalize(self) -> None:
         """
-        
+        Normalize
         """
-        # TODO test
         # NOTE: Assumes that all measures are in grams/milliliters!!!!
         # Calculate total mass of recipe
         sum_: float = 0      # In grams/milliliters
@@ -205,18 +229,28 @@ class Recipe:
 
     def evaluate(self) -> float:
         """
-        
-        """
-        ret = 0
-        # TODO Calculate fitness score
-        # Base it on:
-        # Moistness
-        ret += abs(5 - self._moistness())     # TODO Scaling
-        # Good distribution of types (base, enhancer, binder, etc)
-        # Cohesian of ingredient tastes according to recipe target taste
-        # Quantity of ingredient paired with the ingredient's intensity
+        Calculate the fitness of the recipe. The fitness is a positive,
+        where a good recipe closes to 0. Evaluation is based on:
 
-        return ret
+        Moistness:
+        The resulting mesh from combining the different
+        recipes should contain the right amount of water.
+
+        Function:
+        Penalize not using all types of ingredients.
+
+        Intensity
+        We want balanced tastes. So no extremely salty cookies!
+
+
+        Returns the fitness as a positive float.
+        """
+        fitness = 0
+        fitness += 2 ** abs(self._moistness() - 4)    # TODO Scaling
+        fitness += 2 ** abs(self._intensity() - 5)    # TODO Scaling
+        fitness += 2 ** (len(INGREDIENT_FUNCTIONS) - len(functions))
+
+        return fitness
 
 
 class GA:
@@ -231,15 +265,19 @@ class GA:
         self.database = database
 
 
-    def _init_population(self, population_size: int):
+    def _init_population(self, population_size: int, num_ingredients: int):
         """
-        
+        Initialize a population of recipes. Recipes are given random
+        ingredients, where the amount of ingredients has a gaussian variance
+        around the $num_ingredients parameter. Ingredient quantities are
+        randomly assigned a value in the range [1:1000].
+
+        Returns a list of initialized recipes.
         """
-        # TODO test
         population: list[Recipe] = []
 
         for _ in range(population_size):
-            recipe_size = random.gauss(4, 0.75)     # TODO tweak
+            recipe_size = random.gauss(num_ingredients, 0.75)
             recipe_size = math.ceil(recipe_size)
             ingredients = random.sample(self.database, max(recipe_size, 2))
             child = Recipe(ingredients)
@@ -253,9 +291,11 @@ class GA:
             selection_size: int,
         ) -> list[Recipe]:
         """
+        Select the fittest recipes from the population, 
+        according to the evaluation function.
 
+        Returns a list with the fittest recipe population.
         """
-        # TODO test
         # TODO Do we need ascending or decending????
         evals = [(recipe.evaluate() , recipe) for recipe in population]
         evals.sort(key=lambda tup: tup[0], reverse=False)
@@ -268,9 +308,11 @@ class GA:
             population_size: int,
         ) -> list[Recipe]:
         """
-        
+        Create a new population of new recipes by sampling ingredients
+        from the 2 parents.
+
+        Returns a list containing the new population.
         """
-        # TODO test
         children: list[Recipe] = []
 
         for _ in range(population_size):
@@ -306,7 +348,10 @@ class GA:
             mutation_rate: float = 0.8
         ) -> None:
         """
-        
+        Mutate a recipe by adding/removing ingredients or adjusting
+        the quantity of ingredients used.
+
+        This function is applied in place to the supplied population list.
         """
         for recipe in population:
             recipe.mutate(self.database, mutation_rate)
@@ -315,20 +360,25 @@ class GA:
 
     def normalize(self, population: list[Recipe]) -> None:
         """
-        
+        Normalize all recipes to each weigh 1 kg.
+
+        This function is applied in place to the supplied population list.
         """
-        # TODO test
         for recipe in population:
             recipe.normalize()
 
 
-    def sort_recipes(self, population: Recipe):
+    def sort_recipes(self, population: List[Recipe]):
+        """
+        Sort ingredient list of the recipes alphabetically.
+
+        This function is applied in place to the supplied population list.
+        """
         for recipe in population:
             recipe.sort()
 
 
     def print_recipes(self, recipes: Recipe):
-        # print(recipe)
         for r in recipes:
             print(r.evaluate())
             print(r)
@@ -339,16 +389,16 @@ class GA:
             epochs: int = 100,
             population_size: int = 1,
             selection_size: int = 1,
-            mut_delta: float = 1,
+            num_ingredients: int = 3
         ) -> list[Recipe]:
         """
-        
+        Run the genetic algorithm.
+
+        Return a list of recipes.
         """
-        # TODO test
-        # TODO is mut_delta needed???
         print(
             f"Starting GA with: {epochs} epochs, {population_size} recipes, " \
-            f"{selection_size} selected, delta: {mut_delta}."
+            f"{selection_size} selected."
         )
 
         # Timers
@@ -356,7 +406,7 @@ class GA:
         epoch_time = start_time
 
         # Initialize population
-        population = self._init_population(population_size)
+        population = self._init_population(population_size, num_ingredients)
         self.normalize(population)
 
         # Run algorithm
@@ -375,12 +425,13 @@ class GA:
             self.sort_recipes(population)
             # self.print_recipes(population)
 
-            if i % 10 == 0:
+            if i + 1 % 10 == 0:
                 new_time = time.time()
                 print(
                     f"Epoch {i}, Epoch time taken: {new_time - epoch_time:.4}, " \
                     f"Total time taken: {new_time - start_time:.4}"
                 )
+                # self.print_recipes(population)
                 epoch_time = new_time
         
         print(
@@ -389,40 +440,35 @@ class GA:
         )
 
         # Select final recipes
+        population = self._selection(population, selection_size)
         self.print_recipes(population)
-        return self._selection(population, population_size)
-
+        return population
 
 def main():
-    # TODO Initialize dataset of ingredients
-    # [name, quantity, unit, taste, moistness, type]
-    dataset = [
-        ["water", 500, "ml", "neutral", 10, "moisture agent"],
-        ["milk", 500, "ml", "creamy", 8, "moisture agent"],
-        ["honey", 100, "ml", "sweet",4, "sweetener"],
-        ["butter", 300, "g", "creamy", 8, "fat"],
-        ["margarine", 300, "g", "creamy", 8, "fat"],
-        ["sugar", 300, "g", "sweet", 2, "sweetener"],
-        ["egg", 100, "g", "neutral", 7, "binding agent"],
-        ["flour", 500, "g", "neutral", 1, "dry ingredient"],
-        ["cocoa powder", 100, "g", "bitter", 1, "dry ingredient"],
-        ["salt", 1, "g", "salty", 1, "seasoning"],
-        ["walnuts", 50, "g", "nutty", 1, "mix-in"],
-        ["coconut flakes", 100, "g", "tropical", 1, "mix-in"],
-    ]
+    with open("ingredients_v2.json") as json_file:
+        data = json.load(json_file)
+        dataset = []
 
-    dataset = [Ingredient(i[0], i[1], i[2], i[4], i[3], i[5]) for i in dataset]
+        # Initialize dataset
+        for i in data["cookie_ingredients"]:
+            dataset.append(Ingredient(
+                name =      i["name"],
+                moistness = i["moisture_level"],
+                taste =     i["taste"],
+                function_ = i["type"],
+                intensity = i["intensity"]
+            ))
 
-    # Parameters that tune the genetic algorithm
-    num_ingredients = 3
-    population_size = 100    # Amount of created children each epoch (>2)
-    selection_size  = 20    # Amount of selected children to advance (>2)
-    mut_delta  = 1          # TODO Not sure if needed
-    epochs = 1000
+        # Parameters that tune the genetic algorithm
+        num_ingredients = 5
+        population_size = 100    # Amount of created children each epoch (>2)
+        selection_size  = 20    # Amount of selected children to advance (>2)
+        epochs = 10
 
-    ga = GA(dataset)
-    recipes = ga.run(epochs, population_size, selection_size, mut_delta)
-    # for recipe in recipes:
-    #     recipe.
+        ga = GA(dataset)
+        recipes = ga.run(epochs, population_size, selection_size)
+        # for recipe in recipes:
+        #     recipe.
+
 if __name__ == "__main__":
     main()
